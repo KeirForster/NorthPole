b55d59cd26cf35300bbd60bd8bfe49e70d46c365
Keir Forster <keirforster@gmail.com>
2018-11-12 22:57:42 -0800

    add angular components with default layouts

--- SantaClient/src/app/app-routing.module.ts
+++ SantaClient/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+
+
+const routes: Routes = [
+    { path: 'login', component: LoginComponent, pathMatch: 'full' },
+    { path: 'register', component: RegisterComponent, pathMatch: 'full' },
+    { path: 'home', component: HomeComponent, pathMatch: 'full' },
+    { path: '**', redirectTo: 'home' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
