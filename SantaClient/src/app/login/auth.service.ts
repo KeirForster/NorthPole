3b24dbbe91412632d6f9a2a3786d1aa4d0bb52c4
Keir Forster <keirforster@gmail.com>
2018-11-16 15:10:10 -0800

    Complete SantaClient login (#11)
    * complete basic api endpoints for admin and child
    
    * create angular project
    
    * add angular components with default layouts
    
    * fix SantaClient conflicts
    
    * update api endpoint comments
    
    * update client
    
    * add SantaClient login form
    
    * Add validation for SantaClient login component
    
    * Fix CORS with SantaAPI. Add Error handling and authentication service for SantaClient login component
    
    * Complete SantaClient login with auth service

--- SantaClient/src/app/login/auth.service.ts
+++ SantaClient/src/app/login/auth.service.ts
@@ -3,30 +3,35 @@ import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { catchError, map, tap } from 'rxjs/operators';
 
+import { ApplicationRole } from './application-role';
 import { LoginViewModel } from './login-view-model';
 
 @Injectable({
     providedIn: 'root'
 })
-export class AuthenticateService {
+export class AuthService {
     private static readonly authenticateUrl = 'https://localhost:44302/login';
+
+    private subject: string;
+    private expiration: Date;
+    private roles: ApplicationRole[];
 
     constructor(private http: HttpClient) {}
 
     /**
-     * POST
-     * authenticate the user on the server.
+     * Authenticate a user on the server.
      *
-     * @return an `Observable` of the type `string` for the request.
+     * @param user a user to authenticate
+     * @return an `Observable` of type `string` with a response message.
      */
     authenticate(user: LoginViewModel): Observable<string> {
         return this.http
-            .post(AuthenticateService.authenticateUrl, user, {
+            .post(AuthService.authenticateUrl, user, {
                 observe: 'response'
             })
             .pipe(
-                tap(res => this.setAuthorizationToken(res.body)),
-                map(res => user.email), // pass back the user email
+                tap(res => this.setAuthorizationToken(res.body)), // store id token
+                map(res => 'login success'), // pass back a success message
                 catchError(this.handleError) // handle error
             );
     }
@@ -35,8 +40,36 @@ export class AuthenticateService {
         return localStorage.getItem('token');
     }
 
+    getSubject(): string {
+        return this.subject;
+    }
+
+    getRoles(): ApplicationRole[] {
+        return this.roles;
+    }
+
+    isExpired(): boolean {
+        const now = new Date();
+        const nowMilli = now.getTime();
+        const expMilli =  this.expiration.getTime();
+        return expMilli - nowMilli < 0;
+    }
+
     private setAuthorizationToken(body: any): void {
+        localStorage.removeItem('token');
         localStorage.setItem('token', body.token);
+        this.setApplicationUser(body.token);
+    }
+
+    private setApplicationUser(token: string): void {
+        const payloadBase64Url = token.split('.')[1];
+        const payload = JSON.parse(atob(payloadBase64Url));
+        this.subject = payload.sub;
+        this.roles = payload.roles.split(',');
+
+        const exp = new Date(0); // begining of UTC converted to current timezone
+        exp.setUTCSeconds(payload.exp); // add number of seconds to expiration
+        this.expiration = exp;
     }
 
     private handleError(error: HttpErrorResponse) {
