0fb14dbfb7d9283928fc46f0b030537d92064e23
Keir Forster <keirforster@gmail.com>
2018-11-17 12:40:25 -0800

    Create SantaClient component (#18)

--- SantaClient/src/app/register/register.component.ts
+++ SantaClient/src/app/register/register.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+import { faSync } from '@fortawesome/free-solid-svg-icons';
+import { RegisterViewModel } from './register-view-model';
 
 @Component({
-  selector: 'app-register',
-  templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+    selector: 'app-register',
+    templateUrl: './register.component.html',
+    styleUrls: ['./register.component.scss']
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent {
+    model: RegisterViewModel;
+    confirmPassword: { [value: string]: string };
+    readonly faSync: any; // loading icon
+    submitted: boolean;
+    registerErrorMsg: string;
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+    constructor() {
+        this.model = {} as RegisterViewModel;
+        this.faSync = faSync;
+        this.submitted = false;
+    }
 }
