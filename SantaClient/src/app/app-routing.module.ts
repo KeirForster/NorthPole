e86a86dd7586f15520d9f2d8dc46e8eff0e046c1
Keir Forster <keirforster@gmail.com>
2018-11-18 07:22:48 -0800

    Complete displaying and adding children to Santa's list. (#25)

--- SantaClient/src/app/app-routing.module.ts
+++ SantaClient/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { AdminGuard } from './home/admin.guard';
 import { AuthGuard } from './login/auth.guard';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
@@ -14,6 +15,13 @@ const routes: Routes = [
         component: HomeComponent,
         pathMatch: 'full',
         canActivate: [AuthGuard]
+    },
+    {
+        path: 'home/add-child',
+        component: RegisterComponent,
+        pathMatch: 'full',
+        canActivate: [AdminGuard],
+        data: { redirectUrl: '/home' }
     },
     { path: '**', redirectTo: 'home' }
 ];
--- SantaClient/src/app/home/home.component.ts
+++ SantaClient/src/app/home/home.component.ts
@@ -1,20 +1,58 @@
-import { Component } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { ApplicationUser } from './application-user';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 import { ApplicationRole } from '../login/application-role.enum';
 import { AuthService } from './../login/auth.service';
+import {
+    faPlus,
+    faTrash,
+    faPencilAlt
+} from '@fortawesome/free-solid-svg-icons';
+import { SantaListService } from './santa-list.service';
 
 @Component({
     selector: 'app-home',
     templateUrl: './home.component.html',
     styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit, OnDestroy {
     readonly username: string;
-
     readonly admin: boolean;
+    readonly faPlus;
+    readonly faTrash;
+    readonly faPencilAlt;
+    children: ApplicationUser[];
+    getChildrenError: string;
+    private santaSubscription: Subscription;
 
-    constructor(private authService: AuthService) {
+    constructor(
+        private authService: AuthService,
+        private santaService: SantaListService
+    ) {
         this.username = this.authService.getSubject();
         this.admin = this.authService.isInRole(ApplicationRole.Admin);
+        this.faPlus = faPlus;
+        this.faTrash = faTrash;
+        this.faPencilAlt = faPencilAlt;
+    }
+
+    ngOnInit(): void {
+        if (this.admin) {
+            this.santaSubscription = this.santaService.getChildren().subscribe(
+                (children: ApplicationUser[]) => {
+                    this.children = [...children];
+                },
+                error => {
+                    this.getChildrenError = error;
+                }
+            );
+        }
+    }
+
+    ngOnDestroy(): void {
+        if (this.santaSubscription) {
+            this.santaSubscription.unsubscribe();
+        }
     }
 }
